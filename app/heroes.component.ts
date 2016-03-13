0bf24a9b8053c96626b46f8211f14517432cc540
shinora007 <diablo7271@gmail.com>
2016-03-13 17:38:22 +0530

    adding route from heroes to detail

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from 'angular2/core';
+import { Router } from 'angular2/router';
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
 import {HeroService} from './hero.service';
@@ -6,15 +7,16 @@ import {HeroService} from './hero.service';
 @Component({
     selector: 'my-heroes',
     templateUrl: 'app/heroes.component.html',
-    stylesUrls: ['app/heroes.component.css'],
+    styleUrls: ['app/heroes.component.css'],
     directives: [HeroDetailComponent]
 })
 export class HeroesComponent implements OnInit {
-    title = "Tour of Heroes";
     heroes: Hero[];
     selectedHero: Hero;
 
-    constructor(private _heroService: HeroService) { }
+    constructor(
+    private _router: Router,
+    private _heroService: HeroService) { }
 
     getHeroes() {
         this._heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -25,4 +27,8 @@ export class HeroesComponent implements OnInit {
     }
 
     onSelect(hero: Hero) { this.selectedHero = hero; }
+    
+    gotoDetail() {
+        this._router.navigate(['HeroDetail', { id: this.selectedHero.id }]);
+    }
 }
