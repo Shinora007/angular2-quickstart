4b58e55cfdbbf88573eb76cd0f9f846ab78f955b
shinora007 <diablo7271@gmail.com>
2016-03-13 15:51:42 +0530

    Adding Dashboard

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,10 +2,12 @@ import { Component }       from 'angular2/core';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
 import { HeroService }     from './hero.service';
 import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
+    <a [routerLink]="['Dashboard']">Dashboard</a>
     <a [routerLink]="['Heroes']">Heroes</a>
     <router-outlet></router-outlet>
   `,
@@ -20,6 +22,12 @@ import { HeroesComponent } from './heroes.component';
     path: '/heroes',
     name: 'Heroes',
     component: HeroesComponent
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
   }
 ])
 export class AppComponent {
