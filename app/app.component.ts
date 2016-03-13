c81c5af5417a9673a1afd03356e77577f08e8791
shinora007 <diablo7271@gmail.com>
2016-03-13 14:28:36 +0530

    adding simulation for slow connection

--- app/app.component.ts
+++ app/app.component.ts
@@ -73,17 +73,16 @@ export class AppComponent implements OnInit {
     title = "Tour of Heroes";
     heroes: Hero[];
     selectedHero: Hero;
-    
+
     constructor(private _heroService: HeroService) { }
-    
+
     getHeroes() {
         this._heroService.getHeroes().then(heroes => this.heroes = heroes);
     }
-    
+
     ngOnInit() {
         this.getHeroes();
     }
 
-    
     onSelect(hero: Hero) { this.selectedHero = hero; }
 }
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,9 +1,17 @@
 import {HEROES} from './mock-heroes';
 import {Injectable} from 'angular2/core';
+import {Hero} from './hero';
 
 @Injectable()
 export class HeroService {
   getHeroes() {
     return Promise.resolve(HEROES);
+  }
+  
+  //Like getHeroes, it also returns a promise. But this promise waits 2 seconds before resolving the promise with mock heroes.
+  getHeroesSlowly() {
+    return new Promise<Hero[]>(resolve =>
+      setTimeout(()=>resolve(HEROES), 2000) // 2 seconds
+    );
   }
 }
