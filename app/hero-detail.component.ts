dd1db45157e1593f36c2750149d0b62826df4616
shinora007 <diablo7271@gmail.com>
2016-03-13 16:38:40 +0530

    seperate template file for hero-detail.component

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -6,18 +6,7 @@ import {Hero} from './hero';
 
 @Component({
   selector: 'my-hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-      </div>
-      <button (click)="goBack()">Back</button>
-    </div>
-  `,
-  inputs: ['hero']
+  templateUrl: 'app/hero-detial.component.html',
 })
 export class HeroDetailComponent {
     hero: Hero;
